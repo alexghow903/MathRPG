c86acf24a5dccb6734120a45e5564596e8c7f957
calquinton <91569436+calquinton@users.noreply.github.com>
2022-10-24 14:55:56 -0600

    Update AdditionDialog.gd

--- AdditionDialog/AdditionDialog.gd
+++ AdditionDialog/AdditionDialog.gd
@@ -4,8 +4,25 @@ extends WindowDialog
 var rng = RandomNumberGenerator.new()
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
-	# Randomize rng so it is not the same seed every time
+#func _ready():
+#	# Randomize rng so it is not the same seed every time
+#	rng.randomize()
+#
+#	# randomly generate two numbers
+#	var number_one = rng.randi_range(1, 9)
+#	var number_two = rng.randi_range(1, 9)
+#
+#	# Create string with random numbers assigned
+#	var format_equation = "%s + %s"
+#	var equation = format_equation % [number_one, number_two]
+#
+#	# Set label text as equation string
+#	$MarginContainer/VBoxContainer/EquationLabel.set_text(equation)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+		# Randomize rng so it is not the same seed every time
 	rng.randomize()
 	
 	# randomly generate two numbers
@@ -18,8 +35,3 @@ func _ready():
 	
 	# Set label text as equation string
 	$MarginContainer/VBoxContainer/EquationLabel.set_text(equation)
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
