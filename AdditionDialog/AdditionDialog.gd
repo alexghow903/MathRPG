500a6a663e38b25ac3b555ba7f363256bd7781d8
calquinton <91569436+calquinton@users.noreply.github.com>
2022-10-24 14:46:29 -0600

    Update AdditionDialog.gd
    Updated to generate true random numbers.

--- AdditionDialog/AdditionDialog.gd
+++ AdditionDialog/AdditionDialog.gd
@@ -5,12 +5,18 @@ var rng = RandomNumberGenerator.new()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	# Randomize rng so it is not the same seed every time
+	rng.randomize()
+	
 	# randomly generate two numbers
 	var number_one = rng.randi_range(1, 9)
 	var number_two = rng.randi_range(1, 9)
+	
+	# Create string with random numbers assigned
 	var format_equation = "%s + %s"
 	var equation = format_equation % [number_one, number_two]
 	
+	# Set label text as equation string
 	$MarginContainer/VBoxContainer/EquationLabel.set_text(equation)
 
 
